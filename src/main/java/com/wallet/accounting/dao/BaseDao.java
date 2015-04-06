359a23819a2c15713b0b45eeaf95990ba6ffe245
Kevin Sun <kevin.sun526@qq.com>
2015-04-06 08:48:02 +0800

    base DAO

--- src/main/java/com/wallet/accounting/dao/BaseDao.java
+++ src/main/java/com/wallet/accounting/dao/BaseDao.java
@@ -3,16 +3,17 @@ package com.wallet.accounting.dao;
 import java.io.Serializable;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class BaseDao implements Serializable {
-	private static final long serialVersionUID = 6164882872688463625L;
-	
+public class BaseDao extends SqlSessionDaoSupport implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
 	@Autowired
-	private SqlSessionTemplate sqlSession;
-	
-	public SqlSessionTemplate getSqlSession() {
-		return sqlSession;
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		super.setSqlSessionTemplate(sqlSessionTemplate);
 	}
-	
+
 }
